deb05ea3f669bbdfca17aa63953d8bb5acdca685
Chiem Janssen <chiemjanssen@home.nl>
2020-05-11 14:41:23 +0200

    Updated mysql deployment values and apollo client

--- services/ui/src/lib/apolloClient.ts
+++ services/ui/src/lib/apolloClient.ts
@@ -1,6 +1,35 @@
-import ApolloClient from "apollo-boost";
+import { split } from "apollo-link";
+import { HttpLink } from "apollo-link-http";
+import { WebSocketLink } from "apollo-link-ws";
 import { API_URL } from "./constants";
+import { getMainDefinition } from "apollo-utilities";
+import { ApolloClient } from "apollo-boost";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+const httpLink = new HttpLink({
+  uri: `${API_URL}:3000/graphql`,
+});
+
+const websocketLink = new WebSocketLink({
+  uri: `${API_URL}:5000/`,
+  options: {
+    reconnect: true,
+  },
+});
+
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  websocketLink,
+  httpLink
+);
 
 export const apolloclient = new ApolloClient({
-  uri: API_URL,
+  cache: new InMemoryCache(),
+  link,
 });
